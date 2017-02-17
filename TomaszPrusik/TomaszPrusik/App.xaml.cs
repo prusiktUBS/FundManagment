7069fe529edbd8ae1d9e0e486ed3786ce48f48d0
prusikt <prusikt@gmail.com>
2017-02-17 14:09:42 +0000

    added DI

--- TomaszPrusik/TomaszPrusik/App.xaml.cs
+++ TomaszPrusik/TomaszPrusik/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using TomaszPrusik.DependencyInjection;
 
 namespace TomaszPrusik
 {
@@ -13,5 +14,9 @@ namespace TomaszPrusik
     /// </summary>
     public partial class App : Application
     {
-    }
+		public App()
+		{
+			UnityRegister.Register();
+		}
+	}
 }
---
+++ TomaszPrusik/TomaszPrusik/DependencyInjection/UnityRegister.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainModel;
+using DomainModel.interfaces;
+using Microsoft.Practices.ServiceLocation;
+using Microsoft.Practices.Unity;
+
+namespace TomaszPrusik.DependencyInjection
+{
+	public static class UnityRegister
+	{
+		public static void Register()
+		{
+			IUnityContainer container = new UnityContainer();
+
+			container.RegisterType<IFundService, FundService>();
+			ServiceLocator.SetLocatorProvider( () => new UnityServiceLocator( container ) );
+		}
+	}
+}
